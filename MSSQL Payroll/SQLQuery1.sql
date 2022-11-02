d3ef3096531cc919b21085c5ed972335c7c41a34
Ashish <ashish2000patil@gmail.com>
2022-11-02 16:35:20 +0530

    [Ashish]Added: SumAvgMaxMinCount UC-7

--- MSSQL Payroll/SQLQuery1.sql
+++ MSSQL Payroll/SQLQuery1.sql
@@ -13,3 +13,10 @@ select * from EmployeePayroll where Startdate between cast ('2020-01-01' as date
 alter table EmployeePayroll add Gender char(1) ;
 update EmployeePayroll set Gender='M' where Empname='Rock';
 update EmployeePayroll set Gender='F' where Empname='Nami';
+--UC7-
+select Sum(Salary) as Total_sum from EmployeePayroll where Gender='M' group by Gender;
+select Avg(Salary) as Avg_Salary from EmployeePayroll where Gender='M' group by Gender;
+select min(Salary) as Min_Salary from EmployeePayroll where Gender='M' group by Gender;
+select max(Salary) as Max_Salary from EmployeePayroll where Gender='M' group by Gender;
+select count(*) as Total_Record from EmployeePayroll where Gender='M' group by Gender;
+select count(*) as Total_Record from EmployeePayroll where Gender='F' group by Gender;
