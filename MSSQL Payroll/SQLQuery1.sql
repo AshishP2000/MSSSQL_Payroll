2704bbe2938a8bb83dbfbd77eb46258336496353
Ashish <ashish2000patil@gmail.com>
2022-11-02 07:15:25 +0530

    [Ashish]Added: Insert into Table UC-3

--- MSSQL Payroll/SQLQuery1.sql
+++ MSSQL Payroll/SQLQuery1.sql
@@ -2,4 +2,6 @@
 create database Payroll_Service;
 --UC2-Create Table
 create table EmployeePayroll(ID int identity(1,1) Primary key,Empname varchar(200),Salary bigint,Startdate date);
-
+--UC-3-Insert record
+insert into EmployeePayroll(Empname,Salary,Startdate) values('Rock',25000,'2022-3-4');
+insert into EmployeePayroll(Empname,Salary,Startdate) values('Nami',40000,'2022-03-02');
