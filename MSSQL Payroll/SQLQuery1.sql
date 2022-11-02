9b94a0df2a359e6e96d75f11741429a1ca320636
Ashish <ashish2000patil@gmail.com>
2022-11-02 06:55:06 +0530

    [Ashish]Added: Create Table UC-2

--- MSSQL Payroll/SQLQuery1.sql
+++ MSSQL Payroll/SQLQuery1.sql
@@ -1,2 +1,5 @@
 --UC1-Create Database
 create database Payroll_Service;
+--UC2-Create Table
+create table EmployeePayroll(ID int identity(1,1) Primary key,Empname varchar(200),Salary bigint,Startdate date);
+
